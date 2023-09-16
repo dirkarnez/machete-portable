0fe44d7d55df7ae32aed6bcad302cd6715cba360
Dirk Arnez <smalldirkalex@gmail.com>
2023-09-16 17:29:37 +0800

    - update Machete.exe.Portable.cmd

--- Machete.exe.Portable.cmd
+++ Machete.exe.Portable.cmd
@@ -3,12 +3,18 @@
 echo %PATH%
 
 set USERPROFILE=%~dp0USERPROFILE
+set DESKTOP=%USERPROFILE%\Desktop
 set APPDATA=%USERPROFILE%\AppData\Roaming
 set LOCALAPPDATA=%USERPROFILE%\AppData\Local
 
 echo %USERPROFILE%
+echo %DESKTOP%
 echo %APPDATA%
 echo %LOCALAPPDATA%
+
+if not exist %DESKTOP% (
+	mkdir %DESKTOP%
+)
 
 if not exist %USERPROFILE% (
 	mkdir %USERPROFILE%
